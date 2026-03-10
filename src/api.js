7aacc009426f2bf306401bff545ba8d55f438f1c
Till Klampaeckel <till@php.net>
2026-03-10 18:25:15 +0100

    chore(deps): replace deprecated sodium implementation

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const { Octokit } = require('@octokit/core')
-const sodium = require('tweetsodium')
+const sodium = require('libsodium-wrappers')
 
 /**
  * @class Api
@@ -7,7 +7,6 @@ const sodium = require('tweetsodium')
 module.exports = class Api {
   /**
    * Generate public key to store secrets
-   *
    * @param {any} auth - Auth method
    * @param {string} repo - Repository in format username/repo-name
    * @param {boolean} org - Is a Organization
@@ -21,7 +20,6 @@ module.exports = class Api {
 
   /**
    * Generate public key to store secrets
-   *
    * @returns {Promise<{data: object}>} - Fetch response
    */
   async getPublicKey() {
@@ -35,7 +33,6 @@ module.exports = class Api {
 
   /**
    * Create encrypt secret
-   *
    * @param {string} key_id - Secret key id
    * @param {string} key - Secret key
    * @param {string} name - Secret name
@@ -43,11 +40,12 @@ module.exports = class Api {
    * @returns {Promise<{key_id: string, encrypted_value: string}>} - Secret data
    */
   async createSecret(key_id, key, name, value) {
-    const messageBytes = Buffer.from(value)
+    await sodium.ready
 
+    const messageBytes = Buffer.from(value)
     const keyBytes = Buffer.from(key, 'base64')
 
-    const encryptedBytes = sodium.seal(messageBytes, keyBytes)
+    const encryptedBytes = sodium.crypto_box_seal(messageBytes, keyBytes)
 
     return {
       encrypted_value: Buffer.from(encryptedBytes).toString('base64'),
@@ -57,7 +55,6 @@ module.exports = class Api {
 
   /**
    * Set secret on repository
-   *
    * @param {{encrypted_value:string, key_id:string}} data - Object data to request
    * @param {string} name - Secret name
    * @returns {Promise} - Fetch Response
@@ -73,7 +70,6 @@ module.exports = class Api {
 
   /**
    * Organization checker
-   *
    * @returns {boolean} - Is organization
    */
   isOrg() {
